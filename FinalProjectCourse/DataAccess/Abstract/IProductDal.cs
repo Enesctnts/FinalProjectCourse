ed9607417d8bd7adba12ea12705b9d2f6c58224a
Enes Çetintaş <enesctnts@gmail.com>
2022-02-07 22:28:39 +0300

    FinalProject

---
+++ FinalProjectCourse/DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IProductDal
+    {
+        List<Product> GetALL();
+        void Add(Product product);
+        void Delete(Product product);
+        void Update(Product product);
+        List<Product> GetByAllCategory(int categoryId);
+    }
+}
