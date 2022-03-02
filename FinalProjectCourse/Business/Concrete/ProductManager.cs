b6899f9c4bef1207ea3abd365e8df2d04c1f3753
Enes Çetintaş <enesctnts@gmail.com>
2022-03-02 23:24:37 +0300

    02.03.2022

--- FinalProjectCourse/Business/Concrete/ProductManager.cs
+++ FinalProjectCourse/Business/Concrete/ProductManager.cs
@@ -1,5 +1,8 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Result;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -19,12 +22,9 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
-            if (product.ProductName.Length<2)
-            {
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
             _productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
         }
